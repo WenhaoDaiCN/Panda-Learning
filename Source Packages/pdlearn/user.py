231bfc3adf1d4ecb0b33f571493f67215b5373a9
alivon <mypandalearning.gmail.com>
2019-03-27 22:22:43 +0800

    Signed-off-by: alivon <mypandalearning.gmail.com>

---
+++ Source Packages/pdlearn/user.py
@@ -0,0 +1,63 @@
+import os
+from time import sleep
+from sys import argv
+
+
+def get_user():
+    if len(argv) > 1:
+        uname = argv[1]
+    else:
+        uname = input("输入用户标记名：")
+    if check_uname(uname):
+        dd = check_dd(uname)
+    else:
+        os.makedirs("./user/{}".format(uname))
+        dd = False
+    return dd, uname
+
+
+def check_uname(uname):
+    __check_status = False
+    if os.path.exists("./user/{}".format(uname)):
+        __check_status = True
+    return __check_status
+
+
+def check_dd(uname):
+    __dd_status = False
+    if os.path.exists("./user/{}/dingding".format(uname)):
+        __dd_status = True
+    return __dd_status
+
+
+def get_a_log(uname):
+    __a_log = 0
+    if os.path.exists("./user/{}/a_log".format(uname)):
+        with open("./user/{}/a_log".format(uname), "r", encoding="utf8") as fp:
+            __a_log = int(fp.read())
+    else:
+        with open("./user/{}/a_log".format(uname), "w", encoding="utf8") as fp:
+            fp.write(str(__a_log))
+    return __a_log
+
+
+def get_v_log(uname):
+    __v_log = 0
+    if os.path.exists("./user/{}/v_log".format(uname)):
+        with open("./user/{}/v_log".format(uname), "r", encoding="utf8") as fp:
+            __v_log = int(fp.read())
+    else:
+        with open("./user/{}/v_log".format(uname), "w", encoding="utf8") as fp:
+            fp.write(str(__v_log))
+    return __v_log
+
+
+def shutdown(stime=False):
+    if stime:
+        int(stime)
+        os.system('shutdown -s -t {}'.format(stime))
+        for i in stime:
+            print("\r{}秒后关机".format(i), end="")
+            sleep(1)
+    else:
+        sleep(1800)
---
+++ Source Packages/pdlearn/version.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def up_info():
+    print("正在联网获取跟新信息...")
+    __Version = "v3.0 preview"
+    __INFO = "熊猫学习唯一下载地址为 https://github.com/Alivon/Panda-Learning"
+    try:
+        updata_log = requests.get(
+        "https://raw.githubusercontent.com/Alivon/Panda-Learning/master/Update%20log").content.decode(
+        "utf8")
+        updata_log = updata_log.split("\n")
+        print(__INFO)
+        print("程序版本为：{}，\n最新版本为：{}".format(__Version, updata_log[1].split("=")[1]), end=" ")
+        print("主要更新说明：{}".format(updata_log[2]))
+        print("更新显示不会打断之前操作，请继续...")
+    except:
+        print("网络错误")
+
+
+if __name__ == '__main__':
+    up_info()
