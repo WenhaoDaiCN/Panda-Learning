ffa1c2e566eb02918df8629421157b9fa9e16b40
f02 <49542369+f02@users.noreply.github.com>
2019-04-12 10:25:15 +0800

    Update score.py

--- Source Packages/pdlearn/score.py
+++ Source Packages/pdlearn/score.py
@@ -11,5 +11,6 @@ def get_score(cookies):
     total = int(json.loads(total, encoding="utf8")["data"]["score"])
     each = requests.get("https://pc-api.xuexi.cn/open/api/score/today/queryrate", cookies=jar).content.decode("utf8")
     each = json.loads(each, encoding="utf8")["data"]
+    each = each["dayScoreDtos"] #新增加0412
     each = [int(i["currentScore"]) for i in each if i["ruleId"] in [1, 2, 9, 1002, 1003]]
     return total, each
